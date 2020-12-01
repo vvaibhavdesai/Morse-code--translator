ab822ea743c5e087ff0886dee67d73ef56e31ebb
Vaibhav Desai <vaibhavdesai888@gmail.com>
2020-12-01 12:30:51 +0530

    update morse code translator.

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#text-input");
+var outputDiv= document.querySelector("#output");
+
+var serverURL="https://api.funtranslations.com/translate/morse.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" +text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("We are fixing the server,please trying again after some time!")
+
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+ 
+    fetch(getTranslationURL(inputText))
+       .then(response => response.json())
+       .then(json => {
+          var translatedText = json.contents.translated;
+          outputDiv.innerText = translatedText;
+          })
+       .catch(errorHandler)
+ };
+ 
+ btnTranslate.addEventListener("click", clickHandler)
